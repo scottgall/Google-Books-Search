275c8d72bd3d01ecf8bd38a7ef1df12bd1cb1492
Scott Gall <scottarthurgall@gmail.com>
2019-01-21 20:13:47 -0600

    title, authors, pub, img render on DOM

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ function searchBooks() {
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
     .then((res) => res.json())
     .then((data) => {
+      booksContainer.innerHTML = '';
       Object.entries(data.items).forEach(book => {
         let bookInfo = book[1].volumeInfo;
         console.log(book[1].volumeInfo);
         let bookDiv = document.createElement('div');
         bookDiv.innerHTML = `
-          <p>${bookInfo.authors}</p>
+          <p>${bookInfo.title}</p>
+          <p>${bookInfo.authors.join(', ')}</p>
+          <p>${bookInfo.publisher}</p>
+          <img src='${bookInfo.imageLinks.smallThumbnail}'>
         `;
         document.querySelector('#books-container').appendChild(bookDiv);
       })
@@ -20,5 +24,6 @@ function searchBooks() {
 }
 
 const searchButton = document.querySelector('#search-button');
+const booksContainer = document.querySelector('#books-container');
 
 searchButton.addEventListener('click', searchBooks)
