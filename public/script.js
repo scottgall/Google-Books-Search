dcd18334a5cfd6956d3bbda58ede301935806020
Scott Gall <scottarthurgall@gmail.com>
2019-01-22 20:09:47 -0600

    message appears when no input or no results

--- public/script.js
+++ public/script.js
@@ -1,17 +1,23 @@
 const inputField = document.querySelector('#input-field');
 const searchButton = document.querySelector('#search-button');
 const booksContainer = document.querySelector('#books-container');
+const searchMessage = document.querySelector('#search-message');
 
 function searchBooks() {
+  searchMessage.innerHTML = '';
   let query = document.querySelector('#input-field').value.trim();
     if (query === '') {
-      console.log('enter search keywords');
+      searchMessage.innerHTML = `* enter search keywords *`;
       return;
     }
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
     .then((res) => res.json())
     .then((data) => {
       booksContainer.innerHTML = '';
+      if (data.totalItems === 0) {
+        searchMessage.innerHTML = `* no results found *`;
+        return;
+      }
       Object.entries(data.items).forEach(book => {
         let bookInfo = book[1].volumeInfo;
         let title = bookInfo.title;
@@ -29,6 +35,10 @@ function searchBooks() {
         `;
         document.querySelector('#books-container').appendChild(bookDiv);
       })
+    .catch(err => {
+      console.log(err);
+      throw(error);
+    })
   });
 }
 
